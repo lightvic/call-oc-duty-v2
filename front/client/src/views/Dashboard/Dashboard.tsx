81d0c7d1ece3ff34b78a36bacad159fbc8623835
ValentineLefebvre <v_lefebvre2@hetic.eu>
2023-01-13 14:25:29 +0100

    notre nom et mail dans le menu et le dashboard

--- front/client/src/views/Dashboard/Dashboard.tsx
+++ front/client/src/views/Dashboard/Dashboard.tsx
@@ -1,40 +1,42 @@
 import {
-  FilterButton,
-  Menu,
-  PurchasesContainer,
-  RedirectButton,
+	FilterButton,
+	Menu,
+	PurchasesContainer,
+	RedirectButton,
 } from '../../components'
+import jwt_decode from "jwt-decode"
 
 export default function Dashboard() {
-  const filters = [
-    {
-      value: '12 mois',
-    },
-    {
-      value: '30 jours',
-    },
-    {
-      value: '7 jours',
-    },
-    {
-      value: '24 heures',
-    },
-  ]
-  return (
-    <>
-      <Menu />
-      <div className="dashboard-container">
-        <div className="dashboard-info">
-          <p className="dashboard-info__title">Call Oc Duty dashboard</p>
-          <p>Bon retour, Anthony !</p>
-        </div>
+	const filters = [
+		{
+			value: '12 mois',
+		},
+		{
+			value: '30 jours',
+		},
+		{
+			value: '7 jours',
+		},
+		{
+			value: '24 heures',
+		},
+	]
+	const currentUser = jwt_decode(sessionStorage.token)
+	return (
+		<>
+			<Menu />
+			<div className="dashboard-container">
+				<div className="dashboard-info">
+					<p className="dashboard-info__title">Call Oc Duty dashboard</p>
+					<p>Bon retour, {(currentUser as any).pseudo} !</p>
+				</div>
 
-        <div className="dashboard-buttons">
-          <FilterButton filters={filters} />
-          {/* <RedirectButton src={''} name={'Filter button'} type={'primary'} /> */}
-        </div>
-        {/* <PurchasesContainer /> */}
-      </div>
-    </>
-  )
+				<div className="dashboard-buttons">
+					<FilterButton filters={filters} />
+					{/* <RedirectButton src={''} name={'Filter button'} type={'primary'} /> */}
+				</div>
+				{/* <PurchasesContainer /> */}
+			</div>
+		</>
+	)
 }
