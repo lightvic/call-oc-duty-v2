2f82dd48efa71148132094c2f2fd62ffa2772a83
lightvic <vguillerd@gmail.com>
2023-01-14 00:53:23 +0100

    affichage

---
+++ front/client/src/components/ColocDoughnut/ColocDounghnut.jsx
@@ -0,0 +1,87 @@
+import { $CombinedState } from '@reduxjs/toolkit';
+import React, { PureComponent, useEffect, useState } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import {useNavigate} from "react-router-dom";
+
+
+export default function ColocDounghnut() {
+  const token = JSON.parse(sessionStorage.token)
+  const navigate = useNavigate()
+
+  const [expenses, setExpense]= useState()
+  useEffect(() => {
+    fetch('http://localhost:4557/api/colocStat/42500393-9805-4bca-8b5d-03df0d297927&365', {
+      method: "GET",
+      headers: new Headers({
+        Authorization: "Basic amZnbWFpbC5jb206cGFzc3dvcmQ="
+    })
+  })
+  .then((data) => data.json())
+  .then((json) => {
+    if (json.message === 'invalid cred') {
+      sessionStorage.removeItem('token')
+      navigate('/signin')
+    }
+    setExpense(json.colocExpense)
+  })
+}, [])
+    
+    if (expenses != null){
+      var totalValue = 0
+      const COLORS = []
+      const data = [{ name: 'Nécessités', value: 200 },]
+      console.log(expenses)
+
+      expenses.map((expenses,index) => (
+        data.push({name: expenses.category, value: parseInt(expenses.value)})
+      ));
+    
+      data.forEach((category)=>{
+        if (category.name === 'Charges/Loyer'){
+          COLORS.push('#0088FE')
+        }
+        if (category.name === 'Courses'){
+          COLORS.push('#00C49F')
+        }
+        if (category.name === 'Soirées'){
+          COLORS.push('#FFBB28')
+        }
+        if (category.name === 'Abonnements'){
+          COLORS.push('#FF8042')
+        }
+        if (category.name === 'Nécessités'){
+          COLORS.push('#73BFB8')
+        }
+        if (category.name === 'Autres'){
+          COLORS.push('#E8FCCF')
+        }
+      })
+      data.forEach((value)=>{
+        totalValue = totalValue + value
+      })    
+      return (
+        <div>
+          <PieChart width={800} height={400} >
+            <Pie
+              data={data}
+              cx={120}
+              cy={200}
+              innerRadius={60}
+              outerRadius={80}
+              paddingAngle={0}
+              dataKey="value"
+            >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))};
+            </Pie>
+        </PieChart>
+        {totalValue}
+      </div>
+    );
+  }
+  else{
+    return <h1> pas de dépense</h1>;
+  }
+};
+
---
+++ front/client/src/components/Purchases/ReccurentPurchase/ReccurentPurchase.jsx
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from "react";
+import { IconVector } from "../../../assets"
+import {useNavigate} from "react-router-dom";
+
+export default function ReccurentPurchase() {
+  const token = JSON.parse(sessionStorage.token)
+  const navigate = useNavigate()
+
+//'/api/unFixExpense/{colocUuid}&{limitDate}'
+  const [purchases, setPurchases]= useState()
+  useEffect(() => {
+    fetch('http://localhost:4557/api/fixExpense/44a36f45-010f-4bf7-a7f0-8434108fecd6&365', {
+      method: "GET",
+      headers: new Headers({
+        Authorization: 'Bearer ' + token.token
+    })
+  })
+  .then((data) => data.json())
+  .then((json) => {
+    if (json.message === 'invalid cred') {
+      sessionStorage.removeItem('token')
+      navigate('/signin')
+    }
+    setPurchases(json.unfix)
+  })
+}, [])
+  console.log(purchases)
+  const date = new Date("2023-01-13 21:17:44")
+  console.log(date.getDate())
+  if (purchases != null){
+    const ListMonth = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Sptembre",
+                      "Octobre","Novembre","Décembre"]    
+    // const expenseDate = new Date(purchase.date)
+    return(
+      purchases.map((purchase) =>
+      <button>
+      {purchase.icon} {purchase.name} {new Date(purchase.date).getDate()} {ListMonth[new Date(purchase.date).getMonth()]} {purchase.pseudo} {purchase.value} €
+      </button>
+      )
+    )
+  }
+  else{
+    return null;
+  }
+}
+// single reccurent purchase
+// on click -> open PurchaseInfoModal
